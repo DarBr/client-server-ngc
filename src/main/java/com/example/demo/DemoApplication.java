3a870ae1927b3cc4a988812c016f40d4d9b2cefb
alieniig <s_lienig22@hwr-berlin.de>
2024-04-11 22:19:34 +0200

    test commit andre

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -8,6 +8,8 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @EnableJpaRepositories(basePackages = "com.example.demo")
 public class DemoApplication {
 
+	//startet den Spring Boot Server
+
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
