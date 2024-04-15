742f7b2c9d9b3be6e9fcd98f1e89996344077770
alieniig <s_lienig22@hwr-berlin.de>
2024-04-15 17:36:41 +0200

    clean up

--- src/main/webapp/ngc-frontend/src/app/nutzer-liste/nutzer-liste.component.ts
+++ src/main/webapp/ngc-frontend/src/app/nutzer-liste/nutzer-liste.component.ts
@@ -1,7 +1,6 @@
 import { CommonModule } from '@angular/common'; // Für allgemeine Direktiven wie ngIf, ngFor
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { DataService } from '../app.component';
 
 
@@ -10,51 +9,18 @@ import { DataService } from '../app.component';
   templateUrl: './nutzer-liste.component.html',
   styleUrls: ['./nutzer-liste.component.css'],
   standalone: true, // Dies kennzeichnet die Komponente als standalone
-  imports: [CommonModule, RouterModule] // Hier fügst du benötigte Module hinzu
+  imports: [CommonModule] // Hier fügst du benötigte Module hinzu
 })
 export class NutzerListeComponent implements OnInit {
   nutzer: any[] = [];
   isLoading = true;
 
-  constructor(
-    private http: HttpClient, 
-    private router: Router, 
-    private cdr: ChangeDetectorRef
-  ) {}
+  constructor(private http: HttpClient) { }
 
   ngOnInit() {
-    this.loadData();
-
-    this.router.events.pipe(
-      filter((event): event is NavigationEnd => event instanceof NavigationEnd)
-    ).subscribe({
-      next: (event: NavigationEnd) => {
-        console.log('Navigation Event:', event);
-        if (event.urlAfterRedirects.includes('/nutzer-liste')) {
-          this.loadData();
-        }
-      },
-      error: (err) => {
-        console.error('Fehler beim Abonnieren von Router-Events', err);
-      }
-    });
-  }
-
-  loadData() {
-    console.log('Lade Nutzerdaten...');
-    this.isLoading = true;
-    this.http.get<any[]>('http://localhost:8080/nutzer').subscribe({
-      next: (data) => {
-        console.log('Daten empfangen:', data);
-        this.nutzer = data;
-        this.isLoading = false;
-        this.cdr.detectChanges(); // Erzwingt die Aktualisierung der Ansicht
-      },
-      error: (error) => {
-        console.error('Fehler beim Laden der Nutzer', error);
-        this.isLoading = false;
-        this.cdr.detectChanges(); // Erzwingt die Aktualisierung der Ansicht
-      }
+    this.http.get<any[]>('http://localhost:8080/nutzer').subscribe((data) => {
+      this.nutzer = data; // Speichern Sie die Daten in der nutzer-Eigenschaft
+      this.isLoading = false;
     });
   }
 }
