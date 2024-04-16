aa43d412534a214c20b6358e2e4fc834340365ce
Adrianmuc <adri.mucha2@gmail.com>
2024-04-16 23:00:44 +0200

    Tabs werden nicht immer wieder neu geladen, man kann auch noch anpassen, welche eventuell doch unter welches Bedingungen neu geladen werden sollen

---
+++ src/main/webapp/ngc-frontend/src/app/CustomReuseStrategy.ts
@@ -0,0 +1,36 @@
+
+
+import { ActivatedRouteSnapshot, DetachedRouteHandle, RouteReuseStrategy } from '@angular/router';
+
+export class CustomReuseStrategy implements RouteReuseStrategy {
+  handlers: { [key: string]: DetachedRouteHandle } = {};
+
+  // Entscheidet, ob eine Route wiederverwendbar ist
+  shouldDetach(route: ActivatedRouteSnapshot): boolean {
+    return true; // Du kannst hier eine Bedingung hinzufügen, um bestimmte Routen auszuschließen
+  }
+
+  // Speichert die Route, wenn sie verlassen wird
+  store(route: ActivatedRouteSnapshot, handle: DetachedRouteHandle): void {
+    this.handlers[route.routeConfig!.path!] = handle;
+  }
+
+  // Entscheidet, ob eine gespeicherte Route bei der Navigation wiederhergestellt werden soll
+  shouldAttach(route: ActivatedRouteSnapshot): boolean {
+    return !!route.routeConfig && !!this.handlers[route.routeConfig.path!];
+  }
+
+  // Holt die gespeicherte Route, wenn eine Route wieder angezeigt werden soll
+  retrieve(route: ActivatedRouteSnapshot): DetachedRouteHandle | null {
+    if (!route.routeConfig || route.routeConfig.loadChildren) {
+      return null;
+    }
+    return this.handlers[route.routeConfig.path!];
+  }
+
+  // Entscheidet, ob eine Route im neuen Zustand instanziiert werden soll
+  shouldReuseRoute(future: ActivatedRouteSnapshot, curr: ActivatedRouteSnapshot): boolean {
+    return future.routeConfig === curr.routeConfig;
+  }
+}
+
--- src/main/webapp/ngc-frontend/src/app/app.config.ts
+++ src/main/webapp/ngc-frontend/src/app/app.config.ts
@@ -1,9 +1,14 @@
 import { ApplicationConfig } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { provideRouter, RouteReuseStrategy } from '@angular/router';
+import { CustomReuseStrategy } from './CustomReuseStrategy'; // Stelle sicher, dass der Pfad korrekt ist
 
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideClientHydration()]
+  providers: [
+    provideRouter(routes),
+    provideClientHydration(),
+    { provide: RouteReuseStrategy, useClass: CustomReuseStrategy }
+  ]
 };
