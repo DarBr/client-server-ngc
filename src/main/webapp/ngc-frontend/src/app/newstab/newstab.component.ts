357df17af964c7bcd695222ab2d58ac448609e6e
alieniig <s_lienig22@hwr-berlin.de>
2024-04-29 15:17:14 +0200

    news tab changes

--- src/main/webapp/ngc-frontend/src/app/newstab/newstab.component.ts
+++ src/main/webapp/ngc-frontend/src/app/newstab/newstab.component.ts
@@ -18,10 +18,29 @@ export class NewstabComponent {
   constructor(private http: HttpClient) { }
 
   ngOnInit() {
-    this.http.get<any[]>('https://finnhub.io/api/v1//company-news?symbol=AAPL&from=2023-08-15&to=2023-08-20&token=co5rfg9r01qv77g7nk90co5rfg9r01qv77g7nk9g').subscribe((data) => {
+    this.http.get<any[]>('https://finnhub.io/api/v1//company-news?symbol=AAPL&from=2023-08-15&to=2024-08-20&token=co5rfg9r01qv77g7nk90co5rfg9r01qv77g7nk9g').subscribe((data) => {
       this.news = data; // Speichern Sie die Daten in der nutzer-Eigenschaft
+     
       this.isLoading = false;
     });
+  }
+
+  unixTimeToDateTime(unixTime: number): string {
+    // Unix-Zeitstempel in Millisekunden umwandeln
+    const date = new Date(unixTime * 1000);
+  
+    // Datum- und Zeitformat festlegen (hier als Beispiel: Jahr-Monat-Tag Stunden:Minuten:Sekunden)
+    const options: Intl.DateTimeFormatOptions = {
+      year: 'numeric',
+      month: '2-digit',
+      day: '2-digit',
+      hour: '2-digit',
+      minute: '2-digit',
+      hour12: false, // 24-Stunden-Format
+    };
+  
+    // Datum und Zeit formatieren und zurückgeben
+    return date.toLocaleString('de-DE', options);
   }
 
 }
