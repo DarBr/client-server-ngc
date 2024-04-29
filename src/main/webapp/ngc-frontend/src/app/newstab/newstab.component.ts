706e154693b735fd7c15b1e3bcf0694cd39ecaad
alieniig <s_lienig22@hwr-berlin.de>
2024-04-29 15:59:50 +0200

    suchfunktion implementiert

--- src/main/webapp/ngc-frontend/src/app/newstab/newstab.component.ts
+++ src/main/webapp/ngc-frontend/src/app/newstab/newstab.component.ts
@@ -1,48 +1,54 @@
 import { CommonModule } from '@angular/common';
 import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
 
 @Component({
   selector: 'app-newstab',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './newstab.component.html',
   styleUrl: './newstab.component.css'
 })
 export class NewstabComponent {
-
-
   news: any[] = [];
   isLoading = true;
+  stockSymbol: string = ''; // Variable für das Aktiensymbol
 
   constructor(private http: HttpClient) { }
 
   ngOnInit() {
-    this.http.get<any[]>('https://finnhub.io/api/v1/company-news?symbol=AAPL&from=2023-08-15&to=2024-08-20&token=co5rfg9r01qv77g7nk90co5rfg9r01qv77g7nk9g').subscribe((data) => {
+    // Standardmäßig News für 'META' abrufen
+    
+  }
+
+  searchNews() {
+    // News für das eingegebene Aktiensymbol abrufen
+    this.getNews(this.stockSymbol);
+  }
+
+  getNews(symbol: string) {
+    this.isLoading = true;
+    this.http.get<any[]>(`https://finnhub.io/api/v1/company-news?symbol=${symbol}&from=2023-08-15&to=2024-08-20&token=co5rfg9r01qv77g7nk90co5rfg9r01qv77g7nk9g`).subscribe((data) => {
       this.news = data.map(item => ({
         ...item,
-        datetime: this.unixTimeToDateTime(item.datetime) // Datum formatieren
+        datetime: this.unixTimeToDateTime(item.datetime)
       }));
       this.isLoading = false;
     });
   }
 
   unixTimeToDateTime(unixTime: number): string {
-    // Unix-Zeitstempel in Millisekunden umwandeln
     const date = new Date(unixTime * 1000);
-
-    // Datum- und Zeitformat festlegen (hier als Beispiel: Jahr-Monat-Tag Stunden:Minuten:Sekunden)
     const options: Intl.DateTimeFormatOptions = {
       year: 'numeric',
       month: '2-digit',
       day: '2-digit',
       hour: '2-digit',
       minute: '2-digit',
-      hour12: false, // 24-Stunden-Format
+      hour12: false,
     };
-
-    // Datum und Zeit formatieren und zurückgeben
     return date.toLocaleString('de-DE', options);
   }
-
 }
