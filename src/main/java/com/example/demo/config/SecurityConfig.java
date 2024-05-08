555ff822ae6f83b0a3c1c7b2507f7832d4586983
DarBr <dariobrueck@gmx.de>
2024-05-08 23:25:18 +0200

    Changes für die Sicherheit bei HTTP Requests ohne Token

--- src/main/java/com/example/demo/config/SecurityConfig.java
+++ src/main/java/com/example/demo/config/SecurityConfig.java
@@ -21,7 +21,7 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
             .authorizeHttpRequests(requests -> requests
-                .requestMatchers("/nutzer/**")
+                .requestMatchers("/nutzer/add", "/nutzer/login", "/nutzer/validateToken", "/nutzer/depotidfromtoken", "/nutzer/kontoidfromtoken", "/nutzer/useridfromtoken", "/nutzer/usernamefromtoken")
                 .permitAll()
                 .anyRequest().authenticated())
             .addFilterBefore(jwtAuthenticationFilter, BasicAuthenticationFilter.class)
