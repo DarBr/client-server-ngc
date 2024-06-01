551b756012a8a93bf9b791e4364f0594b87ae4e8
DarBr <dariobrueck@gmx.de>
2024-06-01 12:21:42 +0200

    Nutzer-Liste entfernt und Placeholder beim Kaufen geändert

--- src/main/webapp/ngc-frontend/src/app/app.component.ts
+++ src/main/webapp/ngc-frontend/src/app/app.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterLinkActive, RouterModule, RouterOutlet} from '@angular/router';
-import { NutzerListeComponent } from './nutzer-liste/nutzer-liste.component';
 import { TransaktionListeComponent } from './transaktion-liste/transaktion-liste.component';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { CommonModule } from '@angular/common';
@@ -14,7 +13,7 @@ import { MatDialogModule } from '@angular/material/dialog';
   standalone: true,
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
-  imports: [RouterOutlet, RouterLink, RouterLinkActive, NutzerListeComponent, TransaktionListeComponent, HttpClientModule, CommonModule, FormsModule, MatDialogModule]
+  imports: [RouterOutlet, RouterLink, RouterLinkActive, TransaktionListeComponent, HttpClientModule, CommonModule, FormsModule, MatDialogModule]
 })
 export class AppComponent {
   title = 'ngc-frontend';
--- src/main/webapp/ngc-frontend/src/app/app.routes.ts
+++ src/main/webapp/ngc-frontend/src/app/app.routes.ts
@@ -1,7 +1,6 @@
 import { Routes } from '@angular/router';
 import { TransaktionListeComponent } from './transaktion-liste/transaktion-liste.component';
 import { HomeComponent } from './home/home.component';
-import { NutzerListeComponent } from './nutzer-liste/nutzer-liste.component';
 import { LoginComponent } from './user-login/user-login.component';
 import { AuthGuard } from './AuthGuard';
 import { AktieKaufenComponent } from './aktie-kaufen/aktie-kaufen.component';
@@ -10,7 +9,6 @@ import { NewstabComponent } from './newstab/newstab.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent, canActivate: [AuthGuard]},
-  { path: 'nutzer-liste', component: NutzerListeComponent, canActivate: [AuthGuard]},
   { path: 'transaktion-liste', component: TransaktionListeComponent, canActivate: [AuthGuard]},
   { path: 'login', component: LoginComponent},
   { path: 'kaufen', component: AktieKaufenComponent, canActivate: [AuthGuard]},
