a497199b658e92759fd1dfc5d02cc35a4ab761a3
alieniig <s_lienig22@hwr-berlin.de>
2025-02-11 15:35:14 +0100

    changes webconfig

--- src/main/java/com/example/demo/config/Webconfig.java
+++ src/main/java/com/example/demo/config/Webconfig.java
@@ -1,4 +1,4 @@
-// WebConfig.java in deinem Spring Boot-Projekt
+// WebConfig.java
 package com.example.demo.config;
 
 import org.springframework.context.annotation.Configuration;
@@ -10,11 +10,11 @@ public class Webconfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        // Stelle sicher, dass die URL und der Port mit deiner Angular-Anwendung übereinstimmen.
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:4200", "http://3.142.199.164/")
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedOrigins("http://localhost:4200", "http://3.142.199.164") // Ohne abschließenden "/"
+                .allowedMethods("*") // Erlaubt alle Methoden (GET, POST, PUT, DELETE, OPTIONS usw.)
                 .allowedHeaders("*")
-                .allowCredentials(true);
+                .allowCredentials(true)
+                .maxAge(3600); // Cache-Dauer für Preflight-Anfragen (in Sekunden)
     }
 }
